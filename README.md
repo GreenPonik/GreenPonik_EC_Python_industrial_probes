66a09237a4acd4e191925bf2ac4842c684571f61
MkLHX <mickael.lehoux@gmail.com>
2019-12-23 12:26:52 +0100

    b

---
+++ README.md
@@ -0,0 +1,220 @@
+## GreenPonik_EC.py Library for Raspberry pi
+---------------------------------------------------------
+This is the sample code for 1/2 EC industrial probes
+## Table of Contents
+
+- [## GreenPonik_EC.py Library for Raspberry pi](#greenponikecpy-library-for-raspberry-pi)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Methods](#methods)
+- [Examples](#examples)
+- [Credits](#credits)
+<snippet>
+<content>
+
+## Installation
+
+Dependencies:
+
+[DFRobot_ADS1115](https://github.com/DFRobot/DFRobot_ADS1115/tree/master/RaspberryPi/Python)
+
+Call modules:
+
+```Python
+
+from DFRobot_ADS1115 import ADS1115
+from GreenPonik_EC import GreenPonik_EC
+
+```
+
+or run example directly
+
+```shell
+$> python3 example/EC_Read.py
+
+```
+## Methods
+
+```python
+
+"""
+@brief Init The Analog EC Sensor
+"""
+def begin(self):
+
+"""
+@brief Convert voltage to EC with temperature compensation
+"""
+def readEC(self,voltage,temperature):
+
+"""
+@brief Calibrate the calibration data
+"""
+def calibration(self,voltage,temperature):
+
+"""
+@brief Reset the calibration data to default value
+"""
+def reset(self):
+
+```
+
+## Examples
+EC read => EC_Read.py
+in libs folder
+
+> git clone https://github.com/DFRobot/DFRobot_ADS1115.git
+```Python
+import time
+import sys
+sys.path.insert(0,'../libs/DFRobot_ADS1115/RaspberryPi/Python/')
+sys.path.insert(0,'../src/')
+
+
+from GreenPonik_EC import GreenPonik_EC
+from DFRobot_ADS1115 import ADS1115
+
+ADS1115_REG_CONFIG_PGA_6_144V = 0x00  # 6.144V range = Gain 2/3
+ADS1115_REG_CONFIG_PGA_4_096V = 0x02  # 4.096V range = Gain 1
+ADS1115_REG_CONFIG_PGA_2_048V = 0x04  # 2.048V range = Gain 2 (default)
+ADS1115_REG_CONFIG_PGA_1_024V = 0x06  # 1.024V range = Gain 4
+ADS1115_REG_CONFIG_PGA_0_512V = 0x08  # 0.512V range = Gain 8
+ADS1115_REG_CONFIG_PGA_0_256V = 0x0A  # 0.256V range = Gain 16
+
+ads1115 = ADS1115()
+ec = GreenPonik_EC()
+ec.begin()
+
+def read_ec():
+    global ads1115
+    global ec
+    temperature = 25 # or make your own temperature read method
+    # Set the IIC address
+    ads1115.setAddr_ADS1115(0x48)
+    # Sets the gain and input voltage range.
+    ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+    # Get the Digital Value of Analog of selected channel
+    adc0 = ads1115.readVoltage(0)
+    # Convert voltage to EC with temperature compensation
+    EC = ec.readEC(adc0['r'], temperature)
+    print("Temperature:%.1f ^C EC:%.3f ms/cm " % (temperature, EC))
+    return EC
+
+
+if __name__ == "__main__":
+    while True:
+        read_ec()
+        time.sleep(1)
+
+```
+EC calibration => EC_Calibration.py
+
+in libs folder
+
+> git clone https://github.com/DFRobot/DFRobot_ADS1115.git
+```Python
+import time
+import sys
+sys.path.insert(0,'../libs/DFRobot_ADS1115/RaspberryPi/Python/')
+sys.path.insert(0,'../src/')
+
+from DFRobot_ADS1115 import ADS1115
+from GreenPonik_EC import GreenPonik_EC
+
+ADS1115_REG_CONFIG_PGA_6_144V = 0x00  # 6.144V range = Gain 2/3
+ADS1115_REG_CONFIG_PGA_4_096V = 0x02  # 4.096V range = Gain 1
+ADS1115_REG_CONFIG_PGA_2_048V = 0x04  # 2.048V range = Gain 2 (default)
+ADS1115_REG_CONFIG_PGA_1_024V = 0x06  # 1.024V range = Gain 4
+ADS1115_REG_CONFIG_PGA_0_512V = 0x08  # 0.512V range = Gain 8
+ADS1115_REG_CONFIG_PGA_0_256V = 0x0A  # 0.256V range = Gain 16
+
+ads1115 = ADS1115()
+ec = GreenPonik_EC()
+ec.begin()
+
+
+def calibration():
+    global ads1115
+    global ec
+    temperature = 25 # or make your own temperature read method
+    # Set the IIC address
+    ads1115.setAddr_ADS1115(0x48)
+    # Sets the gain and input voltage range.
+    ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+    # Get the Digital Value of Analog of selected channel
+    adc0 = ads1115.readVoltage(0)
+    return ec.calibration(adc0['r'], temperature)
+
+
+if __name__ == "__main__":
+    while True:
+        calibration()
+        time.sleep(1)
+
+```
+
+read both pH and EC => PH_EC.py
+
+in libs folder
+
+> git clone https://github.com/GreenPonik/GreenPonik_EC_Python_industrial_probres.git
+
+> git clone https://github.com/DFRobot/DFRobot_ADS1115.git
+
+```Python
+import time
+import sys
+sys.path.insert(0,'../libs/DFRobot_ADS1115/RaspberryPi/Python/')
+sys.path.insert(0,'../libs/GreenPonik_PH_Python_industrial_probes/src/')
+sys.path.insert(0,'../src/')
+
+
+ADS1115_REG_CONFIG_PGA_6_144V        = 0x00 # 6.144V range = Gain 2/3
+ADS1115_REG_CONFIG_PGA_4_096V        = 0x02 # 4.096V range = Gain 1
+ADS1115_REG_CONFIG_PGA_2_048V        = 0x04 # 2.048V range = Gain 2 (default)
+ADS1115_REG_CONFIG_PGA_1_024V        = 0x06 # 1.024V range = Gain 4
+ADS1115_REG_CONFIG_PGA_0_512V        = 0x08 # 0.512V range = Gain 8
+ADS1115_REG_CONFIG_PGA_0_256V        = 0x0A # 0.256V range = Gain 16
+
+from DFRobot_ADS1115 import ADS1115
+from GreenPonik_EC import GreenPonik_EC
+from GreenPonik_PH import GreenPonik_PH
+
+ads1115 = ADS1115()
+ec      = GreenPonik_EC()
+ph      = GreenPonik_PH()
+
+ec.begin()
+ph.begin()
+
+
+def read_ph_ec():
+	global ads1115
+	global ec
+	global ph
+	temperature = 25 # or make your own temperature read process
+	#Set the IIC address
+	ads1115.setAddr_ADS1115(0x48)
+	#Sets the gain and input voltage range.
+	ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+	#Get the Digital Value of Analog of selected channel
+	adc0 = ads1115.readVoltage(0)
+	adc1 = ads1115.readVoltage(1)
+	#Convert voltage to EC with temperature compensation
+	EC = ec.readEC(adc0['r'],temperature)
+	PH = ph.readPH(adc1['r'])
+	print("Temperature:%.1f ^C EC:%.2f ms/cm PH:%.2f " %(temperature,EC,PH))
+	return temperature, EC, PH
+
+
+if __name__ == "__main__":
+    while True:
+        read_ph_ec()
+        time.sleep(1)
+
+```
+
+## Credits
+Writter by Mickael Lehoux, from [GreenPonik](https://www.greenponik.com), 2019
+
+based on [GreenPonik_EC_Python](https://github.com/GreenPonik/GreenPonik_EC_Python)
---
+++ src/GreenPonik_EC.py
@@ -0,0 +1,153 @@
+"""
+####################################################################
+####################################################################
+####################################################################
+################ GreenPonik Read EC through Python3 ################
+################ Use only with industrial probes 1/2 ###############
+####################################################################
+####################################################################
+####################################################################
+Based on GreenPonik_EC_Python library
+https://github.com/GreenPonik/GreenPonik_EC_Python
+
+Need DFRobot_ADS1115 library
+https://github.com/DFRobot/DFRobot_ADS1115/tree/master/RaspberryPi/Python
+"""
+
+import time
+import sys
+
+_kvalue = 1.0
+_kvalueLow = 1.0
+_kvalueHigh = 1.0
+_cmdReceivedBufferIndex = 0
+_voltage = 0.0
+_temperature = 25.0
+
+_raw_1413 = 1.200
+_raw_1413_offset = 0.400
+_raw_276 = 2.500
+_raw_276_offset_low = 0.500
+_raw_276_offset_high = 1.000
+_raw_1288 = 11.850
+_raw_1288_offset = 3.650
+
+
+class GreenPonik_EC():
+    def begin(self):
+        global _kvalueLow
+        global _kvalueHigh
+        try:
+            with open('ecdata.txt', 'r') as f:
+                kvalueLowLine = f.readline()
+                kvalueLowLine = kvalueLowLine.strip('kvalueLow=')
+                _kvalueLow = float(kvalueLowLine)
+                kvalueHighLine = f.readline()
+                kvalueHighLine = kvalueHighLine.strip('kvalueHigh=')
+                _kvalueHigh = float(kvalueHighLine)
+        except:
+            self.reset()
+            pass
+
+    def readEC(self, voltage, temperature):
+        global _kvalueLow
+        global _kvalueHigh
+        global _kvalue
+        rawEC = 1000*voltage/820.0/200.0
+        print(">>>current rawEC is: %.3f" % rawEC)
+        valueTemp = rawEC * _kvalue
+        if(valueTemp > 2.5):
+            _kvalue = _kvalueHigh
+        elif(valueTemp < 2.0):
+            _kvalue = _kvalueLow
+        value = rawEC * _kvalue
+        value = value / (1.0+0.0185*(temperature-25.0))
+        return value
+
+    def KvalueTempCalculation(self, compECsolution, voltage):
+        return 820.0*200.0*compECsolution/1000.0/voltage
+
+    def calibration(self, voltage, temperature):
+        rawEC = 1000*voltage/820.0/200.0
+        print(">>>current rawEC is: %.3f" % rawEC)
+        # automated 1.413 buffer solution dection
+        if (rawEC > _raw_1413-_raw_1413_offset and rawEC < _raw_1413-_raw_1413_offset):
+            compECsolution = 1.413*(1.0+0.0185*(temperature-25.0))
+            KValueTemp = self.KvalueTempCalculation(compECsolution, voltage)
+            round(KValueTemp, 2)
+            print(">>>Buffer Solution:1.413us/cm")
+            f = open('ecdata.txt', 'r+')
+            flist = f.readlines()
+            flist[0] = 'kvalueLow=' + str(KValueTemp) + '\n'
+            f = open('ecdata.txt', 'w+')
+            f.writelines(flist)
+            f.close()
+            status_msg = ">>>EC:1.413us/cm Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds"
+            print(status_msg)
+            time.sleep(5.0)
+            cal_res = {'status': 1413,
+                       'kvalue': KValueTemp,
+                       'status_message': status_msg}
+            return cal_res
+        # automated 2.76 buffer solution dection
+        elif (rawEC > _raw_276-_raw_276_offset_low and rawEC < _raw_276-_raw_276_offset_high):
+            compECsolution = 2.76*(1.0+0.0185*(temperature-25.0))
+            KValueTemp = self.KvalueTempCalculation(compECsolution, voltage)
+            round(KValueTemp, 2)
+            print(">>>Buffer Solution:2.76ms/cm")
+            f = open('ecdata.txt', 'r+')
+            flist = f.readlines()
+            flist[1] = 'kvalueHigh=' + str(KValueTemp) + '\n'
+            f = open('ecdata.txt', 'w+')
+            f.writelines(flist)
+            f.close()
+            status_msg = ">>>EC:2.76ms/cm Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds"
+            print(status_msg)
+            time.sleep(5.0)
+            cal_res = {'status': 276,
+                       'kvalue': KValueTemp,
+                       'status_message': status_msg}
+            return cal_res
+        # automated 12.88 buffer solution dection
+        elif (rawEC > _raw_1288-_raw_1288_offset and rawEC < _raw_1288-_raw_1288_offset):
+            compECsolution = 12.88*(1.0+0.0185*(temperature-25.0))
+            KValueTemp = self.KvalueTempCalculation(compECsolution, voltage)
+            print(">>>Buffer Solution:12.88ms/cm")
+            f = open('ecdata.txt', 'r+')
+            flist = f.readlines()
+            flist[1] = 'kvalueHigh=' + str(KValueTemp) + '\n'
+            f = open('ecdata.txt', 'w+')
+            f.writelines(flist)
+            f.close()
+            status_msg = ">>>EC:12.88ms/cm Calibration completed,Please enter Ctrl+C exit calibration in 5 seconds"
+            print(status_msg)
+            time.sleep(5.0)
+            cal_res = {'status': 1288,
+                       'kvalue': KValueTemp,
+                       'status_message': status_msg}
+            return cal_res
+        else:
+            status_msg = ">>>Buffer Solution Error, EC raw: %.3f, Try Again<<<" % rawEC
+            print(status_msg)
+            cal_res = {'status': 9999, 'status_message': status_msg}
+            return cal_res
+
+    def reset(self):
+        _kvalueLow = 1.0
+        _kvalueHigh = 1.0
+        try:
+            f = open('ecdata.txt', 'r+')
+            flist = f.readlines()
+            flist[0] = 'kvalueLow=' + str(_kvalueLow) + '\n'
+            flist[1] = 'kvalueHigh=' + str(_kvalueHigh) + '\n'
+            f = open('ecdata.txt', 'w+')
+            f.writelines(flist)
+            f.close()
+            print(">>>Reset to default parameters<<<")
+        except:
+            f = open('ecdata.txt', 'w')
+            flist = 'kvalueLow=' + str(_kvalueLow) + '\n'
+            flist += 'kvalueHigh=' + str(_kvalueHigh) + '\n'
+            f.writelines(flist)
+            f.close()
+            print(">>>Reset to default parameters<<<")
